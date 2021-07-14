2d9dc1f640c69e5175de74aa0c9be70764b1677c
jshaik49 <jshaik49@gmail.com>
2021-07-14 18:52:13 +0530

    Send otp to the Email and sms from Node.js App using Sendgrid, Dynamic

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const authroute = require('./routes/auth'); 
-const jwt = require('jsonwebtoken');
-
+const sendgridroute = require('./routes/sendgrid');
 const app = express();
+
 const dbURI="mongodb://localhost/authentication";
 app.use(express.json());
 app.use(express.urlencoded());
  
 //To connect to the db
 app.use('/api/auth', authroute);
+app.use('/api/sendgrid', sendgridroute);
 mongoose.connect(dbURI, { useNewUrlParser:true , useUnifiedTopology:true });
 const db = mongoose.connection;
 db.on("error" , (err)=>{console.error(err)});
